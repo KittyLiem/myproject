fac6b816b2d4755bbb70d49bba364071d3f48afc
kitliem <kitty.liem@ccgroup.nl>
2019-01-11 16:41:33 +0100

    Initial commit

---
+++ site/components/src/main/java/nl/kitty/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package nl.kitty.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="myproject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
